7568feba683b793d05c75821250240e7625ec1d1
maquiles <mattaquiles@gmail.com>
2016-10-29 06:37:14 -0400

    Node.js passport
    added passport password encryption for admin and trainer schemas

--- model/adminDataModel.js
+++ model/adminDataModel.js
@@ -4,9 +4,43 @@ var mongoose = require('mongoose'),
 var adminDataSchema = new Schema({
   firstName:   String,
   lastName:    String,
-  username:    String,
-  password:    String,
+  username:    {
+    type:      String,
+    unique:    true,
+    required   true},
+  password:    {
+    type:      String,
+    required:  true},
   signUpDate:  Date
 });
+
+adminDataSchema.pre('save', function (next) {
+    var user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+adminDataSchema.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model('adminDataModel', adminDataSchema);
--- model/trainerDataModel.js
+++ model/trainerDataModel.js
@@ -4,12 +4,46 @@ var mongoose = require('mongoose'),
 var trainerDataSchema = new Schema({
   firstName:   String,
   lastName:    String,
-  username:    String,
-  password:    String,
+  username:    {
+    type:      String,
+    unique:    true,
+    required:  true},
+  password:    {
+    type:      String,
+    required:  true},
   signUpDate:  Date,
   lastActive:  Date,
   numStudents: Number,
   studentLink: Number
 });
+
+trainerDataSchema.pre('save', function (next) {
+    var user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+trainerDataSchema.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model('trainerDataModel', trainerDataSchema);
