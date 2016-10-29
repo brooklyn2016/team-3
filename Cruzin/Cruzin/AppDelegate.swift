e66b06abdc28ccbaabfc1f4322f8ea63d6317717
Michael Langford <mlangfor@stevens.edu>
2016-10-29 09:22:49 -0400

    Merge branch 'dev_scott' into develop
    # Conflicts:
    #
    Cruzin/Cruzin.xcodeproj/project.xcworkspace/xcuserdata/Scott.xcuserdatad
    /UserInterfaceState.xcuserstate

--- Cruzin/Cruzin/AppDelegate.swift
+++ Cruzin/Cruzin/AppDelegate.swift
@@ -13,13 +13,24 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var navigationController:UINavigationController?
+    var defaults = UserDefaults.standard
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.navigationController = self.window?.rootViewController as? UINavigationController
+        self.navigationController?.navigationBar.isHidden = true
+        
+        let loggedIn:Bool = self.isUserLoggedIn()
+        self.defaults.set(loggedIn, forKey: "loggedIn")
+        
         return true
     }
-
+    
+    func isUserLoggedIn() -> Bool {
+        //Check core data for a user object like name and such, if nothing, return false
+        return false
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- Cruzin/Cruzin/ViewController.swift
+++ Cruzin/Cruzin/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (self.defaults.value(forKey: "loggedIn") as! Bool == false) {
+            let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+            self.present(loginViewController, animated: true, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +32,31 @@ class ViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let module = sender as! Int
         let viewController = segue.destination as! LevelViewController
+        var module_name = ""
+        switch (module) {
+        case 1:
+            module_name = "Materials Science"
+            break
+        case 2:
+            module_name = "Newton's Motion (A)"
+            break
+        case 3:
+            module_name = "Newton's Motion (B)"
+            break
+        case 4:
+            module_name = "Body Sciences"
+            break
+        case 5:
+            module_name = "Soundwaves"
+            break
+        case 6:
+            module_name = "Kinesiology"
+            break
+        default:
+            break
+        }
+        
+        viewController.cat_title = module_name
     }
 }
 
