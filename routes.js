41150ac9c4b3f24916070cd0144cbae8f4a09633
maquiles <mattaquiles@gmail.com>
2016-10-28 22:49:01 -0400

    create student data
    function to create a new student in the database using a POST command

--- routes.js
+++ routes.js
@@ -5,7 +5,8 @@ student = require('./model/studentDataModel'),
 trainer = require('./model/trainerDataModel'),
 admin = require('./model/adminDataModel');
 
-
+/*************************************************************************************************************************/
+//API for student database
 //GET all student data using GET
 router.route('/student/data').get(function(req, res){
   student.find(function(err, data){
@@ -15,6 +16,27 @@ router.route('/student/data').get(function(req, res){
     res.json(data);
   });
 });
+
+//create a new student data using POST
+router.route('/student/data').post(function(req, res){
+  student.create({
+    //set values from studentDataModel
+    firstName:   req.body.firstName,
+    lastName:    req.body.lastName,
+    username:    req.body.username,
+    password:    req.body.password,
+    signUpDate:  req.body.signUpDate,
+    lastActive:  req.body.lastActive,
+    trainerLink: req.body.trainerLink,
+    score:       req.body.score,
+    module:      req.body.module,
+    level:       req.body.moduel
+  }, function(err, data){
+    res.send(err);
+  });
+});
+
+
 
 //GET all trainer data using GET
 router.route('/trainer/data').get(function(req, res){
